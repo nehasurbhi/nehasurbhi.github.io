0ec473db21b90942bde10c5fe4c553ea525634d0
nehasurbhi <75774920+nehasurbhi@users.noreply.github.com>
2021-06-25 01:41:58 +0530

    Photography

---
+++ js/testinomial.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#testimonial-slider").testi-carouse({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        navigationText:["",""],
+        slideSpeed:1000,
+        autoPlay:true
+    });
+});
